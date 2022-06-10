ece76bfa5201f2dff40c767048f8db8e60ee3973
dennis.jaszak <dennis.jaszak@descript.de>
2022-06-10 14:24:28 +0200

    File structure and parse missp.dat into json refs #2

--- NLPAug/character/missp_parser.py
+++
@@ -1,6 +0,0 @@
-parsed_dict = {}
-
-with open('missp.dat', 'r') as f:
-    for line in f:
-        if line.startswith('$'):
-            parsed_dict[line.strip('$ ')] = []
---
+++ NLPAug/utilities/missp_parser.py
@@ -0,0 +1,14 @@
+import json
+
+parsed_dict = {}
+
+with open('missp.dat', 'r') as f:
+    for line in f:
+        if line.startswith('$'):
+            current_correct = line.strip('$ ').replace('\n', '')
+            parsed_dict[current_correct] = []
+        else:
+            parsed_dict[current_correct].append(line.replace('\n', ''))
+
+with open('../character/data/missp_data.json', 'w') as f:
+    json.dump(parsed_dict, f)
