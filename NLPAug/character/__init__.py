ccb67016bf23f8d0fba107700df642ef0baa5b7f
djaszak <dennis-j@hotmail.de>
2022-07-06 21:33:10 +0200

    use new packaged files

---
+++ NLPAug/character/__init__.py
@@ -0,0 +1,9 @@
+from .character import (
+    misspell_data,
+    random_switcher_data,
+    mid_randomizer_data,
+    complete_randomizer_data,
+    keyboard_replacer_data,
+    remover_data,
+    inserter_data,
+)
