24b410efc6406bd9f7c76ef681a4832089bf98a4
dennis.jaszak <dennis.jaszak@descript.de>
2022-09-16 16:12:41 +0200

    Base phrase engine

--- nlp_aug/phrase/phrase.py
+++ nlp_aug/phrase/phrase.py
@@ -1,11 +1,16 @@
+from lib2to3.pytree import Base
+from re import sub
 import spacy
+
+from datasets import load_dataset
 
 from nltk.tokenize import TreebankWordDetokenizer
 
+from spacy import displacy
 from spacy.matcher import Matcher
 
 
-class PhraseReplIns:
+class PhraseAugmenter:
     """A class working as an interface for operations that are aiming at either replacing or inserting words by defined rules."""
 
     thesaurus = None
@@ -15,14 +20,10 @@ class PhraseReplIns:
             self.nlp = spacy.load("en_core_web_lg")
         self.matcher = Matcher(self.nlp.vocab)
 
-    # def replacement_rule(self, token: spacy.tokens.token.Token) -> bool:
-    #     """Implement the rule used to replace as well as how to handle the given thesaurus."""
-    #     NotImplementedError
-    #     pass
+    def transformation(self, sent: str) -> str:
+        """Implement the transformation that should be done to the sentence."""
+        return sent
 
-    # def candidate_selection(self, token: spacy.tokens.token.Token) -> str:
-    #     """Implement the statistical approach which specific synonym from a list should be used."""
-    #     pass
 
     def engine(self, data: str, augmented_feature: str) -> str:
         """Using `replacement_rule()` and `synonym_selection()` a string is augmented
@@ -37,17 +38,30 @@ class PhraseReplIns:
         """
         data_feature = data[augmented_feature]
         doc = self.nlp(data_feature)
+        sents = doc.sents
 
         new_doc = []
 
-        for token in doc:
-            if self.replacement_rule(token):
-                replacement = self.candidate_selection(token)
-                replacement = replacement.replace("_", " ")
-                new_doc.append(replacement)
-            else:
-                new_doc.append(token.text)
+        for sent in sents:
+            new_doc.append(self.transformation(sent))
 
-        d = TreebankWordDetokenizer()
-        data[augmented_feature] = d.detokenize(new_doc)
+        data[augmented_feature] = new_doc
+
         return data
+
+class BaseCropper(PhraseAugmenter):
+    def transformation(self, sent: str, dependency_focus: str = 'nsubj') -> str:
+        sent = super().transformation(sent)
+
+        subj_list = [token for token in sent if token.dep_ == dependency_focus]
+        head_list = [token.head for token in subj_list]
+        
+        print(subj_list)
+        print(head_list)
+
+        return sent
+
+imdb_dataset = load_dataset("imdb", split="train").select(range(10))
+cropper = BaseCropper()
+for data in imdb_dataset:
+    print(cropper.engine(data, augmented_feature='text'))
