1935dc206e22c92c29c46eab955214acf26f95a2
Dennis Jaszak <dennis-j@hotmail.de>
2022-09-18 14:04:00 +0200

    refactor word2vec builder

--- nlp_aug/word/word2vec_builder.py
+++ nlp_aug/word/word2vec_builder.py
@@ -1,31 +1,24 @@
-# Download dataset
-imdb_dataset = load_dataset("imdb", split="train")
-data = []
-t = TreebankWordTokenizer()
-for sent in imdb_dataset["text"]:
-    new_sent = []
-    for word in t.tokenize(sent):
-        # for word in nlp(sentence):
-        # print(word)
-        new_word = word.lower()
-        if new_word[0] not in string.punctuation:
-            new_sent.append(new_word)
-    if len(new_sent) > 0:
-        data.append(new_sent)
-# model = Word2Vec(
-#     sentences = data,
-#     vector_size = 50,
-#     window = 10,
-#     epochs = 20,
-# )
+class Word2VecBuilder:
+    def __init__(data):
+        self.data = data
 
-# model.save("word2vec.model")
+    def build(name: str):
+        data = []
+        t = TreebankWordTokenizer()
+        for sent in data:
+            new_sent = []
+            for word in t.tokenize(sent):
+                new_word = word.lower()
+                if new_word[0] not in string.punctuation:
+                    new_sent.append(new_word)
+            if len(new_sent) > 0:
+                data.append(new_sent)
+                
+        model = Word2Vec(
+            sentences = data,
+            vector_size = 50,
+            window = 10,
+            epochs = 20,
+        )
 
-model = Word2Vec.load("word2vec.model")
-
-nlp = spacy.load("en_core_web_lg")
-data_feature = imdb_dataset['text'][0]
-
-doc = nlp(data_feature)
-
-
+        model.save(f"{name}.model")
